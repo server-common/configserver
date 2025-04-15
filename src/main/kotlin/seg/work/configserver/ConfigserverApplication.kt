d33305757237212ef680c38304d39d9e9b5e809a
seoeungi <sjlim02255@gmail.com>
2025-04-15 15:32:54 +0900

    eureka 관련 설정 주석처리

--- src/main/kotlin/seg/work/configserver/ConfigserverApplication.kt
+++ src/main/kotlin/seg/work/configserver/ConfigserverApplication.kt
@@ -2,11 +2,10 @@ package seg.work.configserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.config.server.EnableConfigServer
 
 @EnableConfigServer
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 class ConfigserverApplication
 
